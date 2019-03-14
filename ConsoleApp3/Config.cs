24235f42c68bccd7bad9d97f31ab84f8060edc28
GOTTschalkrobin <43066708+GOTTschalkrobin@users.noreply.github.com>
2019-03-14 22:05:27 +0100

    TestBot erstellt, welcher auf Server connecten kann

---
+++ ConsoleApp3/Config.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ConsoleApp3
+{
+    class Config
+    {
+        private const string configFolder = "Resources";
+        private const string configFile = "config.json";
+        private const string folderPath = configFolder + "/" + configFile;
+
+        public static BotConfig bot;
+
+        static Config()
+        {
+            if (!Directory.Exists(configFolder))
+                Directory.CreateDirectory(configFolder);
+            if(!File.Exists(folderPath))
+            {
+                bot = new BotConfig();
+                string json = JsonConvert.SerializeObject(bot, Formatting.Indented);
+                File.WriteAllText(folderPath, json);
+            }
+            else
+            {
+                string json = File.ReadAllText(folderPath);
+                bot = JsonConvert.DeserializeObject<BotConfig>(json);
+                
+            }
+
+        }
+    }
+    public struct BotConfig
+    {
+        public string token;
+        public string cmdPrefix;
+    }
+}
---
+++ ConsoleApp3/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ConsoleApp3
+{
+    class Utilities
+    {
+        private static Dictionary<string, string> alerts;
+
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLang/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>();
+        }
+
+        public static string GetAlert(string key)
+        {
+            if (alerts.ContainsKey(key)) return alerts[key];
+            return "";
+            {
+
+            }
+        }
+    }
+}
